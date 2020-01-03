3bc987777176d5d4f95ab38afc313fd7438b6e19
Vasa <35889432+poidynets@users.noreply.github.com>
2020-01-03 16:35:34 +0100

    Update and rename SerchBarController.js to SearchBarController.js

--- SearchBarController.js
+++ SearchBarController.js
@@ -1,7 +1,7 @@
 ({
 	searchKeyChange : function(component, event, helper)
     {
-		var myEvent=$A.get("searchKeyChange");
+        var myEvent=$A.get("e.c:searchKeyChange");
         myEvent.setParams({"searchKey":event.target.value});
         myEvent.fire();
 	}
