7b659732c53e0749f05c30326f215747ff787c36
Vasa <35889432+poidynets@users.noreply.github.com>
2020-01-03 15:04:36 +0100

    Create ContactListController.js

---
+++ ContactListController.js
@@ -0,0 +1,10 @@
+({
+    doInit : function(component, event) {
+        var action=component.get("c.findAll");
+        action.setCallback(this,function(a)
+        {
+                           component.set("v.contacts",a.getReturnValue());
+    });
+    $A.enqueueAction(action);
+}
+ })
